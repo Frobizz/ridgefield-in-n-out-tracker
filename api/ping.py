15333ae42145455d73269c3a75a603e2e1873c8f
Branden Scalera <frobizz@gmail.com>
2025-08-13 12:56:58 -0700

    Add /api/ping to verify Python function wiring

---
+++ api/ping.py
@@ -0,0 +1,9 @@
+from http.server import BaseHTTPRequestHandler
+import json
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        body = json.dumps({"ok": True, "msg": "pong"}).encode()
+        self.send_response(200)
+        self.send_header("content-type","application/json")
+        self.end_headers()
+        self.wfile.write(body)
